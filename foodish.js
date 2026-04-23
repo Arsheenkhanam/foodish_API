5a71c4a5f8c700506c2f1144dd3dc36a09620339
Arsheenkhanam <arsheenkhanam369@gmail.com>
2026-04-23 19:56:22 +0530

    Remove unused comments from foodish.js
    Removed commented API URLs from foodish.js.

--- foodish.js
+++ foodish.js
@@ -4,5 +4,3 @@ async function getimg(){
     const result=finaldata.image;
     document.getElementById("img").innerHTML=`<img src="${result}" alt="image of food items" width="250"/>`;
 }
-//  https://foodish-api.com/api/
-// https://forms.gle/K888R9nV8bm6PLfZ9
